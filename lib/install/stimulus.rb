e364233663ef1db91fd1e38ffe6c6c4c5ad13d05
David Heinemeier Hansson <david@loudthinking.com>
2020-12-09 17:53:09 +0100

    Install Stimulus files into app

---
+++ lib/install/stimulus.rb
@@ -0,0 +1,8 @@
+say "Copying Stimulus JavaScript"
+directory "#{__dir__}/app/assets/javascripts", "app/assets/javascripts"
+
+say "Add app/javascripts to asset pipeline manifest"
+append_to_file Rails.root.join("app/assets/config/manifest.js").to_s, "//= link_tree ../javascripts\n"
+
+say "Add Stimulus include tags in application layout"
+insert_into_file Rails.root.join("app/views/layouts/application.html.erb").to_s, "\n    <%= stimulus_include_tags %>", before: /\s*<\/head>/
--- lib/tasks/stimulus_tasks.rake
+++ lib/tasks/stimulus_tasks.rake
@@ -1,4 +1,6 @@
-# desc "Explaining what the task does"
-# task :hotwire_rails do
-#   # Task goes here
-# end
+namespace :stimulus do
+  desc "Explaining what the task does"
+  task :install do
+    system "#{RbConfig.ruby} ./bin/rails app:template LOCATION=#{File.expand_path("../install/stimulus.rb", __dir__)}"
+  end
+end
