79add84448ebf3aae5ff9f165b581ff207182916
David Heinemeier Hansson <david@loudthinking.com>
2020-12-09 15:47:14 +0100

    Stimulus is all about controllers, no need to repeat that

--- app/helpers/stimulus/stimulus_helper.rb
+++ app/helpers/stimulus/stimulus_helper.rb
@@ -1,10 +1,10 @@
 module Stimulus::StimulusHelper
-  def stimulus_include_tags(loading = :autoload)
+  def stimulus_include_tags(loader = :autoloader)
     [
       javascript_include_tag("stimulus/libraries/es-module-shims", type: "module"),
       tag.script(type: "importmap-shim", src: asset_path("importmap.json")),
       javascript_include_tag("stimulus/libraries/stimulus", type: "module-shim"),
-      javascript_include_tag("stimulus/loaders/#{loading}_controllers", type: "module-shim")
+      javascript_include_tag("stimulus/loaders/#{loader}", type: "module-shim")
     ].join("\n").html_safe
   end
 end
