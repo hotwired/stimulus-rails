e0d85748c3ffd644ccf3dd45df663ddb4fc2e465
David Heinemeier Hansson <david@loudthinking.com>
2020-12-15 14:17:12 +0100

    Enhance autoloader to work with multiple controllers in a single data-controller attribute

--- app/assets/javascripts/stimulus/loaders/autoloader.js
+++ app/assets/javascripts/stimulus/loaders/autoloader.js
@@ -4,12 +4,15 @@ const application = Application.start()
 
 function autoload() {
   Array.from(document.querySelectorAll('[data-controller]')).forEach((element) => {
-    let controllerName = element.attributes["data-controller"].value
-    let controllerFilename = `${controllerName}_controller`
+    const controllerNames = element.attributes["data-controller"].value.split(" ")
 
-    import(controllerFilename).then((controllerModule) => {
-      application.register(controllerName, controllerModule.default)
-    }).catch(error => console.log(error))
+    controllerNames.forEach((controllerName) => {
+      let controllerFilename = `${controllerName}_controller`
+
+      import(controllerFilename).then((controllerModule) => {
+        application.register(controllerName, controllerModule.default)
+      }).catch(error => console.log(`Failed to autoload controller: ${controllerName}`))
+    })
   })
 }
 
