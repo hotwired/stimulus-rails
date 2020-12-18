6bd712c60157cf2e2e4c43e347bd8654e203c861
David Heinemeier Hansson <david@loudthinking.com>
2020-12-18 18:33:09 +0100

    Need to remove the mini profiler

--- lib/install/stimulus.rb
+++ lib/install/stimulus.rb
@@ -12,3 +12,4 @@ comment_lines Rails.root.join("config/environments/development.rb"), /config.ass
 
 say "Turn off rack-mini-profiler"
 comment_lines Rails.root.join("Gemfile"), /rack-mini-profiler/
+run "bin/bundle"
