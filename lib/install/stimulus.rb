066c5f24a4b6e35ff7dbfe6b68cc398186c07735
David Heinemeier Hansson <david@loudthinking.com>
2020-12-18 16:18:57 +0100

    Turn off debug aids that conflict with showing a clean run

--- lib/install/stimulus.rb
+++ lib/install/stimulus.rb
@@ -6,3 +6,9 @@ append_to_file Rails.root.join("app/assets/config/manifest.js").to_s, "//= link_
 
 say "Add Stimulus include tags in application layout"
 insert_into_file Rails.root.join("app/views/layouts/application.html.erb").to_s, "\n    <%= stimulus_include_tags %>", before: /\s*<\/head>/
+
+say "Turn off development debug mode"
+comment_lines Rails.root.join("config/environments/development.rb"), /config.assets.debug = true/
+
+say "Turn off rack-mini-profiler"
+comment_lines Rails.root.join("Gemfile"), /rack-mini-profiler/
