0ff55ecb4786206fac5631820058b3d875ed139f
Justin Malčić <j.malcic@me.com>
2020-12-24 14:43:29 +0000

    Add controllers in subdirectories to importmap

--- lib/stimulus/importmap_helper.rb
+++ lib/stimulus/importmap_helper.rb
@@ -1,3 +1,5 @@
+require 'find'
+
 module Stimulus::ImportmapHelper
   def importmap_list_with_stimulus_from(*paths)
     [ %("stimulus": "#{asset_path("stimulus/libraries/stimulus")}"), importmap_list_from(*paths) ].join(",\n")
@@ -8,16 +10,19 @@ module Stimulus::ImportmapHelper
       absolute_path = Rails.root.join(path)
       dirname       = absolute_path.basename.to_s
 
-      absolute_path.children.collect do |module_filename|
-        module_name = importmap_module_name_from(module_filename)
-        %("#{module_name}": "#{asset_path("#{dirname}/#{module_filename.basename}")}")
+      Find.find(absolute_path).each_with_object([]) do |path, entries|
+        next if FileTest.directory?(path)
+
+        relative_pathname = Pathname.new(path).relative_path_from(absolute_path)
+        module_name = importmap_module_name_from(relative_pathname)
+        entries << %("#{module_name}": "#{asset_path("#{dirname}/#{relative_pathname}")}")
       end
     end.join(",\n")
   end
 
   private
     # Strip off the extension and any versioning data for an absolute module name.
-    def importmap_module_name_from(filename)
-      filename.basename.to_s.remove(filename.extname).split("@").first
+    def importmap_module_name_from(relative_pathname)
+      relative_pathname.to_s.remove(relative_pathname.extname).split("@").first
     end
 end
