e72c5bb688e54f4ef36ef3ab7846f60dc13872e0
Kohei SUGI <kohei.sg@gmail.com>
2020-12-26 01:35:48 +0900

    Make runnable test
    Rename to stimulus-rails from hotfire-rails in
    `test/dummy/config/applciation`.
    Add sqlite3 dependency in Gemfile not gemspec.

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -5,7 +5,7 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-require "hotwire-rails"
+require "stimulus-rails"
 
 module Dummy
   class Application < Rails::Application
