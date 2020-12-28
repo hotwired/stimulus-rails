453771f85d19275e9a7414dbb1e3176a9ac19391
Justin Malčić <j.malcic@me.com>
2020-12-28 11:23:26 +0000

    Refactor autoloader into class

--- app/assets/javascripts/stimulus/loaders/autoloader.js
+++ app/assets/javascripts/stimulus/loaders/autoloader.js
@@ -1,22 +1,28 @@
 import { Application } from "stimulus"
 
-const application = Application.start()
+class Autoloader {
+  constructor() {
+    this.application = Application.start()
+  }
 
-function autoload() {
-  Array.from(document.querySelectorAll('[data-controller]')).forEach((element) => {
-    const controllerNames = element.attributes["data-controller"].value.split(" ")
-
-    controllerNames.forEach((controllerName) => {
-      let underscoredControllerName = controllerName.replace(/--/g, "/").replace(/-/g, "_")
-      let controllerFilename = `${underscoredControllerName}_controller`
-
-      import(controllerFilename).then((controllerModule) => {
-        application.register(controllerName, controllerModule.default)
-      }).catch(error => console.log(`Failed to autoload controller: ${controllerName}`))
+  autoload() {
+    Array.from(document.querySelectorAll('[data-controller]')).forEach((element) => {
+      const controllerNames = element.attributes["data-controller"].value.split(" ")
+  
+      controllerNames.forEach((controllerName) => {
+        let underscoredControllerName = controllerName.replace(/--/g, "/").replace(/-/g, "_")
+        let controllerFilename = `${underscoredControllerName}_controller`
+  
+        import(controllerFilename).then((controllerModule) => {
+          this.application.register(controllerName, controllerModule.default)
+        }).catch(error => console.log(`Failed to autoload controller: ${controllerName}`))
+      })
     })
-  })
+  }
 }
 
-autoload()
+const autoloader = new Autoloader()
 
-window.addEventListener("turbo:load", autoload)
+autoloader.autoload()
+
+window.addEventListener("turbo:load", autoloader.autoload)
