0f5558992ddcbe17de368fd4e874abe86566b678
Justin Malčić <j.malcic@me.com>
2020-12-28 13:34:36 +0000

    Make require more granular

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,6 +1,10 @@
 require_relative "boot"
 
-require "rails/all"
+require "rails"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
--- test/dummy/config/environments/production.rb
+++ test/dummy/config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "dummy_production"
 
-  config.action_mailer.perform_caching = false
+  # config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -95,7 +95,7 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  # config.active_record.dump_schema_after_migration = false
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
