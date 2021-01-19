57994b817e0f6121f3d183b82f308ba6adacfabe
David Heinemeier Hansson <david@loudthinking.com>
2021-01-19 16:50:29 +0100

    Revised names and function composition

--- app/assets/javascripts/stimulus/loaders/autoloader.js
+++ app/assets/javascripts/stimulus/loaders/autoloader.js
@@ -2,40 +2,44 @@ import { Application } from "stimulus"
 
 const application = Application.start()
 const { controllerAttribute } = application.schema
+const registeredControllers = {}
 
-const loaded = {}
-
-function autoloadControllers(element) {
-  controllerNamesInElement(element).forEach(loadController)
+function autoloadControllersWithin(element) {
+  queryControllerNamesWithin(element).forEach(loadController)
 }
 
-function controllerNamesInElement(element) {
-  const elements = Array.from(element.querySelectorAll(`[${controllerAttribute}]`))
-  return elements.map(e => e.getAttribute(controllerAttribute).split(/\s+/).filter(content => content.length)).flat()
+function queryControllerNamesWithin(element) {
+  return Array.from(element.querySelectorAll(`[${controllerAttribute}]`)).map(extractControllerNamesFrom).flat()
 }
 
-function register(name, module) {
-  if (name in loaded) return
-
-  application.register(name, module.default)
-
-  loaded[name] = true
+function extractControllerNamesFrom(element) {
+  return element.getAttribute(controllerAttribute).split(/\s+/).filter(content => content.length)
 }
 
 function loadController(name) {
-  const filename = name.replace(/--/g, "/").replace(/-/g, "_")
-
-  import(`${filename}_controller`)
-    .then(module => register(name, module))
+  import(controllerFilename(name))
+    .then(module => registerController(name, module))
     .catch(error => console.log(`Failed to autoload controller: ${name}`, error))
 }
+
+function controllerFilename(name) {
+  return `${name.replace(/--/g, "/").replace(/-/g, "_")}_controller`
+}
+
+function registerController(name, module) {
+  if (name in registeredControllers) return
+
+  application.register(name, module.default)
+  registeredControllers[name] = true
+}
+
 
 new MutationObserver((mutationsList) => {
   for (const { attributeName, target } of mutationsList) {
     if (attributeName == controllerAttribute && target.hasAttribute(controllerAttribute)) {
-      autoloadControllers(target)
+      autoloadControllersWithin(target)
     }
   }
 }).observe(document.body, { attributeFilter: [controllerAttribute], subtree: true, childList: true })
 
-autoloadControllers(document)
+autoloadControllersWithin(document)
