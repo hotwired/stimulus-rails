6bd6a7254404f37cfde448caf31a4c3b296ac5ae
David Heinemeier Hansson <david@loudthinking.com>
2021-01-19 16:56:42 +0100

    Improving naming and composition

--- lib/stimulus/importmap_helper.rb
+++ lib/stimulus/importmap_helper.rb
@@ -6,11 +6,10 @@ module Stimulus::ImportmapHelper
   def importmap_list_from(*paths)
     Array(paths).flat_map do |path|
       if (absolute_path = Rails.root.join(path)).exist?
-        dirname = absolute_path.basename
-        Dir[absolute_path.join("**/*.js{,m}")].collect { |file| Pathname.new(file) }.select(&:file?).collect do |filename|
+        find_javascript_files_in_tree(absolute_path).collect do |filename|
           module_filename = filename.relative_path_from(absolute_path)
-          module_name = importmap_module_name_from(module_filename)
-          module_path = asset_path(dirname.join(module_filename))
+          module_name     = importmap_module_name_from(module_filename)
+          module_path     = asset_path(absolute_path.basename.join(module_filename))
 
           %("#{module_name}": "#{module_path}")
         end
@@ -22,5 +21,9 @@ module Stimulus::ImportmapHelper
     # Strip off the extension and any versioning data for an absolute module name.
     def importmap_module_name_from(filename)
       filename.to_s.remove(filename.extname).split("@").first
+    end
+
+    def find_javascript_files_in_tree(path)
+      Dir[path.join("**/*.js{,m}")].collect { |file| Pathname.new(file) }.select(&:file?)
     end
 end
