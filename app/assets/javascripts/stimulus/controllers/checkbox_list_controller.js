798b5a26071e6d1a3bacc20392e8994cfbc6732d
Matt Swanson <swan3788@gmail.com>
2021-01-20 19:58:42 -0500

    Add checkbox-list controller

---
+++ app/assets/javascripts/stimulus/controllers/checkbox_list_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  // Actions
+
+  checkAll() {
+    this.setAllCheckboxes(true)
+  }
+
+  checkNone() {
+    this.setAllCheckboxes(false)
+  }
+
+  // Private
+
+  setAllCheckboxes(checked) {
+    this.checkboxes.forEach((el) => {
+      const checkbox = el
+
+      if (!checkbox.disabled) {
+        checkbox.checked = checked
+      }
+    });
+  }
+
+  get checkboxes() {
+    return this.element.querySelectorAll("input[type=checkbox]")
+  }
+}
