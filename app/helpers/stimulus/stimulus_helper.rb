b53feef53b6dab3b63be56e2608de4dc20d53100
Alberto Vena <kennyadsl@gmail.com>
2021-06-04 14:51:18 +0200

    Allow including importmap from custom location (#41)
    This is needed when using the importmap provided by some Engine.
    The path of the importmap is probably namespaced under some
    directories to avoid collisions with other Engines.

--- app/helpers/stimulus/stimulus_helper.rb
+++ app/helpers/stimulus/stimulus_helper.rb
@@ -1,8 +1,8 @@
 module Stimulus::StimulusHelper
-  def stimulus_include_tags
+  def stimulus_include_tags(importmap = "importmap.json")
     safe_join [
       javascript_include_tag("stimulus/libraries/es-module-shims", type: "module"),
-      tag.script(type: "importmap-shim", src: asset_path("importmap.json")),
+      tag.script(type: "importmap-shim", src: asset_path(importmap)),
       javascript_include_tag("stimulus/libraries/stimulus", type: "module-shim"),
       javascript_include_tag("stimulus/loaders/autoloader", type: "module-shim")
     ], "\n"
