11cf514448ba5a974872a1a8945b86174e0ec40f
David Heinemeier Hansson <david@loudthinking.com>
2021-07-28 18:18:41 -0400

    Use same order as elsewhere

--- app/helpers/stimulus/stimulus_helper.rb
+++ app/helpers/stimulus/stimulus_helper.rb
@@ -2,9 +2,9 @@ module Stimulus::StimulusHelper
   def stimulus_include_tags(importmap = "importmap.json")
     safe_join [
       javascript_include_tag("stimulus/libraries/es-module-shims", type: "module"),
-      tag.script(type: "importmap-shim", src: asset_path(importmap)),
       javascript_include_tag("stimulus/libraries/stimulus", type: "module"),
       javascript_include_tag("stimulus/loaders/autoloader", type: "module")
+      tag.script(src: asset_path(importmap), type: "importmap-shim"),
     ], "\n"
   end
 end
