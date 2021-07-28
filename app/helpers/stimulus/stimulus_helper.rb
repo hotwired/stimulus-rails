bce3260c807be259585f70161a7fb3a51a7523a9
David Heinemeier Hansson <david@loudthinking.com>
2021-07-28 17:14:11 -0400

    Use polyfill strategy for include tags

--- app/helpers/stimulus/stimulus_helper.rb
+++ app/helpers/stimulus/stimulus_helper.rb
@@ -3,8 +3,8 @@ module Stimulus::StimulusHelper
     safe_join [
       javascript_include_tag("stimulus/libraries/es-module-shims", type: "module"),
       tag.script(type: "importmap-shim", src: asset_path(importmap)),
-      javascript_include_tag("stimulus/libraries/stimulus", type: "module-shim"),
-      javascript_include_tag("stimulus/loaders/autoloader", type: "module-shim")
+      javascript_include_tag("stimulus/libraries/stimulus", type: "module"),
+      javascript_include_tag("stimulus/loaders/autoloader", type: "module")
     ], "\n"
   end
 end
