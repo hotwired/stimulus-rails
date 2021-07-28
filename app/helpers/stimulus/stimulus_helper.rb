3a5de8f3f1674bd6dec0178a31a5e6dec63ce3b1
David Heinemeier Hansson <david@loudthinking.com>
2021-07-28 18:19:03 -0400

    Need to stick with shims as long as we want an external importmap

--- app/helpers/stimulus/stimulus_helper.rb
+++ app/helpers/stimulus/stimulus_helper.rb
@@ -2,9 +2,9 @@ module Stimulus::StimulusHelper
   def stimulus_include_tags(importmap = "importmap.json")
     safe_join [
       javascript_include_tag("stimulus/libraries/es-module-shims", type: "module"),
-      javascript_include_tag("stimulus/libraries/stimulus", type: "module"),
-      javascript_include_tag("stimulus/loaders/autoloader", type: "module")
       tag.script(src: asset_path(importmap), type: "importmap-shim"),
+      javascript_include_tag("stimulus/libraries/stimulus", type: "module-shim"),
+      javascript_include_tag("stimulus/loaders/autoloader", type: "module-shim")
     ], "\n"
   end
 end
