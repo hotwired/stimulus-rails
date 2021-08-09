f37b0ca2e942ecf2e84afe5c72a38871ddf67841
David Heinemeier Hansson <david@loudthinking.com>
2021-08-09 17:34:57 +0200

    Add stimulus assets to the importmap if one is available

--- lib/stimulus/engine.rb
+++ lib/stimulus/engine.rb
@@ -5,5 +5,14 @@ module Stimulus
         Rails.application.config.assets.precompile += %w( stimulus stimulus-autoloader )
       end
     end
+
+    initializer "stimulus.importmap" do
+      if Rails.application.config.respond_to?(:importmap)
+        Rails.application.config.importmap.paths.tap do |paths|
+          paths.asset "stimulus"
+          paths.asset "stimulus-autoloader"
+        end
+      end
+    end
   end
 end
