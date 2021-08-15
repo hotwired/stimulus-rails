b495fbfdef433ae8c7ea04f809b2effded16cc6f
David Heinemeier Hansson <david@loudthinking.com>
2021-08-15 21:13:36 +0200

    Add extra CR to ensure we don't append to existing line

--- lib/install/stimulus_with_asset_pipeline.rb
+++ lib/install/stimulus_with_asset_pipeline.rb
@@ -2,7 +2,7 @@ APP_JS_ROOT = Rails.root.join("app/assets/javascripts")
 IMPORTMAP_PATH = Rails.root.join("config/initializers/importmap.rb")
 
 say "Import Stimulus autoloader in existing app/assets/javascripts/application.js"
-append_to_file APP_JS_ROOT.join("application.js"), %(import "@hotwired/stimulus-autoloader"\n)
+append_to_file APP_JS_ROOT.join("application.js"), %(\nimport "@hotwired/stimulus-autoloader"\n)
 
 say "Creating controllers directory"
 copy_file "#{__dir__}/app/assets/javascripts/controllers/hello_controller.js", APP_JS_ROOT.join("controllers/hello_controller.js")
