dcbcee0b5f9784c8c52cfaa7441cfd141d96e5df
David Heinemeier Hansson <david@loudthinking.com>
2021-08-15 21:13:15 +0200

    Append to app importmap instead of engine configuration
    More explicit, which is needed now that we preload by default

--- lib/install/stimulus_with_asset_pipeline.rb
+++ lib/install/stimulus_with_asset_pipeline.rb
@@ -1,7 +1,19 @@
 APP_JS_ROOT = Rails.root.join("app/assets/javascripts")
+IMPORTMAP_PATH = Rails.root.join("config/initializers/importmap.rb")
 
 say "Import Stimulus autoloader in existing app/assets/javascripts/application.js"
 append_to_file APP_JS_ROOT.join("application.js"), %(import "@hotwired/stimulus-autoloader"\n)
 
 say "Creating controllers directory"
 copy_file "#{__dir__}/app/assets/javascripts/controllers/hello_controller.js", APP_JS_ROOT.join("controllers/hello_controller.js")
+
+if IMPORTMAP_PATH.exist?
+  insert_into_file \
+    IMPORTMAP_PATH.to_s, 
+    %(  pin "@hotwired/stimulus", to: "stimulus.js"\n  pin "@hotwired/stimulus-autoloader", to: "stimulus-autoloader.js"\n\n),
+    after: "Rails.application.config.importmap.draw do\n"
+else
+  say <<~INSTRUCTIONS, :green
+    You must add @rails/actiontext and trix to your importmap to reference them via ESM.
+  INSTRUCTIONS
+end
--- lib/stimulus/engine.rb
+++ lib/stimulus/engine.rb
@@ -5,14 +5,5 @@ module Stimulus
         Rails.application.config.assets.precompile += %w( stimulus stimulus-autoloader )
       end
     end
-
-    initializer "stimulus.importmap" do
-      if Rails.application.config.respond_to?(:importmap)
-        Rails.application.config.importmap.draw do
-          pin "@hotwired/stimulus", to: "stimulus.js"
-          pin "@hotwired/stimulus-autoloader", to: "stimulus-autoloader.js"
-        end
-      end
-    end
   end
 end
