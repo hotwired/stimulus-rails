a61a4d8605da67f1b2c8a245ebe87714ce5511b7
David Heinemeier Hansson <david@loudthinking.com>
2021-08-16 12:38:24 +0200

    Add an autoloader for importmap
    Pulls in everything under controller/ in the importmap for autoloading.

---
+++ app/assets/javascripts/stimulus-importmap-autoloader.js
@@ -0,0 +1,14 @@
+import { Application } from "@hotwired/stimulus"
+
+const application = Application.start()
+
+const importmap = await JSON.parse(document.querySelector("script[type=importmap]").text)
+const importedControllerPaths = Object.keys(importmap.imports).filter((e) => e.match("controllers/"))
+
+importedControllerPaths.forEach(function(path) {
+  const name = path.replace("controllers/", "").replace("_controller", "").replace("/", "--").replace("_", "-")
+
+  import(path)
+    .then(module => application.register(name, module.default))
+    .catch(error => console.log(`Failed to autoload controller: ${name}`, error))
+})
