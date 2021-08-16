71aab2bf9c43e18b193965f24b359dde375f5f40
David Heinemeier Hansson <david@loudthinking.com>
2021-08-16 12:56:15 +0200

    Setup to install in app/javascript instead

--- lib/install/stimulus_with_asset_pipeline.rb
+++ lib/install/stimulus_with_asset_pipeline.rb
@@ -1,20 +1,27 @@
-APP_JS_ROOT = Rails.root.join("app/assets/javascripts")
+APP_JS_ROOT = Rails.root.join("app/javascript")
+APP_JS_PATH = APP_JS_ROOT.join("application.js")
 IMPORTMAP_PATH = Rails.root.join("config/initializers/importmap.rb")
 
-say "Import Stimulus autoloader in existing app/assets/javascripts/application.js"
-append_to_file APP_JS_ROOT.join("application.js"), %(\nimport "@hotwired/stimulus-autoloader"\n)
+if APP_JS_PATH.exist?
+  say "Import Stimulus autoloader in existing app/javascript/application.js"
+  append_to_file APP_JS_PATH, %(\nimport "@hotwired/stimulus-autoloader"\n)
+else
+  say <<~INSTRUCTIONS, :red
+    You must import @hotwire/stimulus and @hotwire/stimulus-importmap-autoloader in your application.js.
+  INSTRUCTIONS
+end
 
 say "Creating controllers directory"
-copy_file "#{__dir__}/app/assets/javascripts/controllers/hello_controller.js", APP_JS_ROOT.join("controllers/hello_controller.js")
+copy_file "#{__dir__}/app/javascript/controllers/hello_controller.js", APP_JS_ROOT.join("controllers/hello_controller.js")
 
 if IMPORTMAP_PATH.exist?
-  say "Pin @hotwired/stimulus and @hotwired/stimulus-autoloader in config/initializers/importmap.rb"
+  say "Pin @hotwired/stimulus and @hotwired/stimulus-importmap-autoloader in config/initializers/importmap.rb"
   insert_into_file \
     IMPORTMAP_PATH.to_s, 
-    %(  pin "@hotwired/stimulus", to: "stimulus.js"\n  pin "@hotwired/stimulus-autoloader", to: "stimulus-autoloader.js"\n\n),
+    %(  pin "@hotwired/stimulus", to: "stimulus.js"\n  pin "@hotwired/stimulus-importmap-autoloader", to: "stimulus-importmap-autoloader.js"\n\n),
     after: "Rails.application.config.importmap.draw do\n"
 else
-  say <<~INSTRUCTIONS, :green
+  say <<~INSTRUCTIONS, :red
     You must add @rails/actiontext and trix to your importmap to reference them via ESM.
   INSTRUCTIONS
 end
--- lib/install/stimulus_with_webpacker.rb
+++ lib/install/stimulus_with_webpacker.rb
@@ -2,7 +2,7 @@ say "Appending Stimulus setup code to #{Webpacker.config.source_entry_path}/appl
 append_to_file "#{Webpacker.config.source_entry_path}/application.js", %(\nimport "controllers")
 
 say "Creating controllers directory"
-directory "#{__dir__}/app/assets/javascripts/controllers", "#{Webpacker.config.source_path}/controllers"
+directory "#{__dir__}/app/javascript/controllers", "#{Webpacker.config.source_path}/controllers"
 
 say "Using Stimulus NPM package"
 gsub_file "#{Webpacker.config.source_path}/controllers/hello_controller.js", /@hotwired\//, ''
