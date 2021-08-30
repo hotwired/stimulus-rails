87f2cb68f045fe1769928ea5d1143ba5d036415c
David Heinemeier Hansson <david@loudthinking.com>
2021-08-30 15:28:22 +0200

    Just precompile one file that'll serve as the manifest for anything else we might need

--- app/assets/javascripts/stimulus.js
+++ app/assets/javascripts/stimulus.js
@@ -1,2 +1,4 @@
 //= require ./stimulus@2.js
 //= link ./stimulus@3.0.0-beta.1.js
+//= link ./stimulus-autoloader.js
+//= link ./stimulus-importmap-autoloader.js
--- lib/stimulus/engine.rb
+++ lib/stimulus/engine.rb
@@ -2,7 +2,7 @@ module Stimulus
   class Engine < ::Rails::Engine
     initializer "stimulus.assets" do
       if Rails.application.config.respond_to?(:assets)
-        Rails.application.config.assets.precompile += %w( stimulus stimulus-autoloader stimulus-importmap-autoloader )
+        Rails.application.config.assets.precompile += %w( stimulus )
       end
     end
   end
