c36406a03a125d246d8e8c1db89b5aacdcce5217
David Heinemeier Hansson <david@loudthinking.com>
2021-09-02 19:53:36 +0200

    Tailor install for importmap/webpacker/node
    Also reveal config options

--- lib/install/stimulus_with_importmap.rb
+++ lib/install/stimulus_with_importmap.rb
@@ -1,10 +1,18 @@
 say "Create controllers directory"
-directory "#{__dir__}/app/javascript/controllers", "app/javascript/controllers"
+empty_directory "app/javascript/controllers"
+copy_file "#{__dir__}/app/javascript/controllers/index_for_importmap.js",
+  "app/javascript/controllers/index.js"
+copy_file "#{__dir__}/app/javascript/controllers/hello_controller.js",
+  "app/javascript/controllers/hello_controller.js"
 
-say "Import Stimulus importmap autoloader in existing app/javascript/application.js"
+say "Import Stimulus importmap autoloader in app/javascript/application.js"
 append_to_file "app/javascript/application.js",
   %(import "@hotwired/stimulus-importmap-autoloader"\n)
 
-say "Pin Stimulus dependencies"
-append_to_file "config/importmap.rb",
-  %(pin "@hotwired/stimulus", to: "stimulus.js"\npin "@hotwired/stimulus-importmap-autoloader", to: "stimulus-importmap-autoloader.js"\npin_all_from "app/javascript/controllers", under: "controllers"\n)
+say "Pin Stimulus"
+append_to_file "config/importmap.rb" do <<-RUBY
+  pin "@hotwired/stimulus", to: "stimulus.js"
+  pin "@hotwired/stimulus-importmap-autoloader", to: "stimulus-importmap-autoloader.js"
+  pin_all_from "app/javascript/controllers", under: "controllers"
+RUBY
+end
--- lib/install/stimulus_with_node.rb
+++ lib/install/stimulus_with_node.rb
@@ -1,8 +1,19 @@
 say "Create controllers directory"
-directory "#{__dir__}/app/javascript/controllers", "app/javascript/controllers"
+empty_directory "app/javascript/controllers"
 
-say "Import Stimulus controllers in app/javascript/application.js"
-append_to_file "app/javascript/application.js", %(import "controllers"\n)
+if Rails.root.join("config/webpacker.yml").exist?
+  copy_file "#{__dir__}/app/javascript/controllers/index_for_webpacker.js",
+    "app/javascript/controllers/index.js"
+else
+  copy_file "#{__dir__}/app/javascript/controllers/index_for_node.js",
+    "app/javascript/controllers/index.js"
+end
 
-say "Install Stimulus dependencies"
+copy_file "#{__dir__}/app/javascript/controllers/hello_controller.js",
+  "app/javascript/controllers/hello_controller.js"
+
+say "Import Stimulus controllers"
+append_to_file "app/javascript/application.js", %(import "./controllers"\n)
+
+say "Install Stimulus"
 run "npm i @hotwired/stimulus"
