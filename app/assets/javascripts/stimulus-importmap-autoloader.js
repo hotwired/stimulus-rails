d38f7c714e0165161c45fb838092f5d45ed5ba9f
David Heinemeier Hansson <david@loudthinking.com>
2021-09-04 12:55:00 +0200

    Simplify the autoloader setup

--- app/assets/javascripts/stimulus-importmap-autoloader.js
+++ app/assets/javascripts/stimulus-importmap-autoloader.js
@@ -1,11 +1,15 @@
 // FIXME: es-module-shim won't shim the dynamic import without this explicit import
 import "@hotwired/stimulus"
 
-export function registerControllersFrom(under, importmap, application) {
-  const paths = Object.keys(importmap)
+export function registerControllersFrom(under, application) {
+  const paths = Object.keys(parseImportmapJson())
     .filter(path => path.match(new RegExp(`^${under}/.*_controller$`)))
 
   paths.forEach(path => registerControllerFromPath(path, under, application))
+}
+
+export function parseImportmapJson() {
+  return JSON.parse(document.querySelector("script[type=importmap]").text).imports
 }
 
 function registerControllerFromPath(path, under, application) {
