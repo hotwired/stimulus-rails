06931fa4299e4bce3389714724a192ae8767e287
David Heinemeier Hansson <david@loudthinking.com>
2021-09-12 14:22:30 +0200

    Split out producing one import and register combo so you can use that directly for chunking

--- lib/stimulus/manifest.rb
+++ lib/stimulus/manifest.rb
@@ -3,16 +3,20 @@ module Stimulus::Manifest
 
   def generate_from(controllers_path)
     extract_controllers_from(controllers_path).collect do |controller_path|
-      module_path = controller_path.relative_path_from(controllers_path).to_s.remove(".js")
-      controller_class_name = module_path.camelize.gsub(/::/, "__")
-      tag_name = module_path.remove("_controller").gsub(/\//, "--")
-  
-      <<-JS
+      import_and_register_controller(controllers_path, controller_path)
+    end
+  end
+
+  def import_and_register_controller(controllers_path, controller_path)
+    module_path = controller_path.relative_path_from(controllers_path).to_s.remove(".js")
+    controller_class_name = module_path.camelize.gsub(/::/, "__")
+    tag_name = module_path.remove("_controller").gsub(/\//, "--")
+
+    <<-JS
 
 import #{controller_class_name} from "./#{module_path}"
 application.register("#{tag_name}", #{controller_class_name})
-      JS
-    end
+    JS
   end
 
   def extract_controllers_from(directory)
