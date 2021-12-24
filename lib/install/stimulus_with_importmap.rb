97ed6574e3a8ad1e8e100211d7679dacd5637615
Sam Ruby <rubys@intertwingly.net>
2021-12-24 09:01:04 -0500

    replace hello controller with a show confirm example
    complete with scaffolding

--- lib/install/stimulus_with_importmap.rb
+++ lib/install/stimulus_with_importmap.rb
@@ -4,8 +4,8 @@ copy_file "#{__dir__}/app/javascript/controllers/index_for_importmap.js",
   "app/javascript/controllers/index.js"
 copy_file "#{__dir__}/app/javascript/controllers/application.js",
   "app/javascript/controllers/application.js"
-copy_file "#{__dir__}/app/javascript/controllers/hello_controller.js",
-  "app/javascript/controllers/hello_controller.js"
+copy_file "#{__dir__}/app/javascript/controllers/show_controller.js",
+  "app/javascript/controllers/show_controller.js"
 
 say "Import Stimulus controllers"
 append_to_file "app/javascript/application.js", %(import "controllers"\n)
--- lib/stimulus/engine.rb
+++ lib/stimulus/engine.rb
@@ -5,5 +5,10 @@ module Stimulus
         Rails.application.config.assets.precompile += %w( stimulus.js stimulus.min.js stimulus.min.js.map )
       end
     end
+
+    initializer "stimulus.generator_templates" do
+      next unless Rails::VERSION::MAJOR >= 7
+      Rails.application.config.generators.templates.unshift(File.expand_path('../templates', __dir__))
+    end
   end
 end
