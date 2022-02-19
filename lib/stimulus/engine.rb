b7935348d30091c0de7cf5e24aa57254e9141503
Kirill Usanov <kirill.usanov.dev@gmail.com>
2022-02-20 02:25:24 +0300

    Precompile assets constant for opt out possibility

--- lib/stimulus/engine.rb
+++ lib/stimulus/engine.rb
@@ -1,8 +1,16 @@
 module Stimulus
   class Engine < ::Rails::Engine
+    # If you don't want to precompile Stimulus's assets (e.g., you're using jsbundling),
+    # you can do this in an initializer:
+    #
+    # config.after_initialize do
+    #   config.assets.precompile -= Stimulus::Engine::PRECOMPILE_ASSETS
+    # end
+    PRECOMPILE_ASSETS = %w( stimulus.js stimulus.min.js stimulus.min.js.map ).freeze
+
     initializer "stimulus.assets" do
       if Rails.application.config.respond_to?(:assets)
-        Rails.application.config.assets.precompile += %w( stimulus.js stimulus.min.js stimulus.min.js.map )
+        Rails.application.config.assets.precompile += PRECOMPILE_ASSETS
       end
     end
   end
