26754fe0a4fa0893f191521e93a7e89592f78a0a
Sunny Ripert <sunny@sunfox.org>
2022-07-18 23:39:17 +0200

    Remove `.js` extension from JavaScript imports (#104)

--- test/manifest_test.rb
+++ test/manifest_test.rb
@@ -6,11 +6,15 @@ class Stimulus::Manifest::Test < ActiveSupport::TestCase
     manifest = Stimulus::Manifest.generate_from(file_fixture('controllers')).join
 
     # JavaScript controller
-    assert_includes manifest, 'import HelloController from "./hello_controller.js"'
+    assert_includes manifest, 'import HelloController from "./hello_controller"'
     assert_includes manifest, 'application.register("hello", HelloController)'
 
     # CoffeeScript controller
-    assert_includes manifest, 'import CoffeeController from "./coffee_controller.coffee"'
+    assert_includes manifest, 'import CoffeeController from "./coffee_controller"'
     assert_includes manifest, 'application.register("coffee", CoffeeController)'
+
+    # TypeScript controller
+    assert_includes manifest, 'import TypeScriptController from "./type_script_controller"'
+    assert_includes manifest, 'application.register("type-script", TypeScriptController)'
   end
 end
