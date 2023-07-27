6b1fd8f1ab5720b140bb164d3e830d59efd1df28
Kevin Berthier <berthier.kevin.y@gmail.com>
2023-07-27 13:47:05 +0200

    Add skip_manifest option to generator (#123)

--- lib/generators/stimulus/stimulus_generator.rb
+++ lib/generators/stimulus/stimulus_generator.rb
@@ -3,10 +3,12 @@ require "rails/generators/named_base"
 class StimulusGenerator < Rails::Generators::NamedBase # :nodoc:
   source_root File.expand_path("templates", __dir__)
 
+  class_option :skip_manifest, type: :boolean, default: false, desc: "Don't update the stimulus manifest"
+
   def copy_view_files
     @attribute = stimulus_attribute_value(controller_name)
     template "controller.js", "app/javascript/controllers/#{controller_name}_controller.js"
-    rails_command "stimulus:manifest:update" unless Rails.root.join("config/importmap.rb").exist?
+    rails_command "stimulus:manifest:update" unless Rails.root.join("config/importmap.rb").exist? || options[:skip_manifest]
   end
 
   private
