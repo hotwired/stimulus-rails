d90cf81b3505dcf740d9c5aec9362710d1eeae7f
Joseph Hirn <joseph.hirn@gmail.com>
2025-02-03 13:42:14 -0600

    Support js_root generator option for controller and manifest tasks.

--- lib/generators/stimulus/stimulus_generator.rb
+++ lib/generators/stimulus/stimulus_generator.rb
@@ -4,10 +4,11 @@ class StimulusGenerator < Rails::Generators::NamedBase # :nodoc:
   source_root File.expand_path("templates", __dir__)
 
   class_option :skip_manifest, type: :boolean, default: false, desc: "Don't update the stimulus manifest"
+  class_option :js_root, type: :string, default: 'app/javascript', desc: "Root path for javascript files"
 
   def copy_view_files
     @attribute = stimulus_attribute_value(controller_name)
-    template "controller.js", "app/javascript/controllers/#{controller_name}_controller.js"
+    template "controller.js", "#{options[:js_root]}/controllers/#{controller_name}_controller.js"
     rails_command "stimulus:manifest:update" unless Rails.root.join("config/importmap.rb").exist? || options[:skip_manifest]
   end
 
