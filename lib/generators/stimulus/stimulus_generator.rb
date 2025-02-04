66187f77b17053f807d766b8b7b4139e2888a80a
Joseph Hirn <joseph.hirn@gmail.com>
2025-02-04 13:00:00 -0600

    Use .js_root instead place of hash accessor

--- lib/generators/stimulus/stimulus_generator.rb
+++ lib/generators/stimulus/stimulus_generator.rb
@@ -2,7 +2,6 @@ require "rails/generators"
 require "rails/generators/actions"
 require "rails/generators/named_base"
 
-
 class StimulusGenerator < Rails::Generators::NamedBase # :nodoc:
   source_root File.expand_path("templates", __dir__)
 
@@ -11,7 +10,7 @@ class StimulusGenerator < Rails::Generators::NamedBase # :nodoc:
 
   def copy_view_files
     @attribute = stimulus_attribute_value(controller_name)
-    template "controller.js", "#{options[:js_root]}/controllers/#{controller_name}_controller.js"
+    template "controller.js", "#{options.js_root}/controllers/#{controller_name}_controller.js"
     rails_command "stimulus:manifest:update" unless Rails.root.join("config/importmap.rb").exist? || options[:skip_manifest]
   end
 
