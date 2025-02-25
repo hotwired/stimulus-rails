98e8a2c0db54a6f8ec905f0bb8499ca072007cf9
David Heinemeier Hansson <david@hey.com>
2025-02-25 10:39:35 +0100

    Use a fully formed controllers_path

--- lib/generators/stimulus/stimulus_generator.rb
+++ lib/generators/stimulus/stimulus_generator.rb
@@ -7,11 +7,11 @@ class StimulusGenerator < Rails::Generators::NamedBase # :nodoc:
   source_root File.expand_path("templates", __dir__)
 
   class_option :skip_manifest, type: :boolean, default: false, desc: "Don't update the stimulus manifest"
-  class_option :js_root, type: :string, default: "app/javascript", desc: "Root path for javascript files"
+  class_option :controllers_path, type: :string, default: "app/javascript/controllers", desc: "Root path for controller files"
 
   def copy_view_files
     @attribute = stimulus_attribute_value(controller_name)
-    template "controller.js", "#{options.js_root}/controllers/#{controller_name}_controller.js"
+    template "controller.js", "#{options.controllers_path}/#{controller_name}_controller.js"
     rails_command "stimulus:manifest:update" unless update_manifest_index?
   end
 
