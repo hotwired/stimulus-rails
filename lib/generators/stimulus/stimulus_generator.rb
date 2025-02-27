d247817453eee91af06efe0feb342641228fe7cb
David Heinemeier Hansson <david@37signals.com>
2025-02-27 16:39:16 +0100

    Move index writing into a Manifest method to avoid generator failure
    It also improves the code!

--- lib/generators/stimulus/stimulus_generator.rb
+++ lib/generators/stimulus/stimulus_generator.rb
@@ -1,4 +1,5 @@
 require "rails/generators/named_base"
+require "stimulus/manifest"
 
 class StimulusGenerator < Rails::Generators::NamedBase # :nodoc:
   source_root File.expand_path("templates", __dir__)
@@ -8,7 +9,7 @@ class StimulusGenerator < Rails::Generators::NamedBase # :nodoc:
   def copy_view_files
     @attribute = stimulus_attribute_value(controller_name)
     template "controller.js", "app/javascript/controllers/#{controller_name}_controller.js"
-    rails_command "stimulus:manifest:update" unless Rails.root.join("config/importmap.rb").exist? || options[:skip_manifest]
+    Stimulus::Manifest.write_index_from(Rails.root.join("app/javascript/controllers")) unless update_manifest_index?
   end
 
   private
@@ -18,5 +19,9 @@ class StimulusGenerator < Rails::Generators::NamedBase # :nodoc:
 
     def stimulus_attribute_value(controller_name)
       controller_name.gsub(/\//, "--").gsub("_", "-")
+    end
+
+    def update_manifest_index?
+      !(Rails.root.join("config/importmap.rb").exist? || options[:skip_manifest])
     end
 end
